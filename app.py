b00e791bacf2586807bfac094c78f4bf379941fa
nidhinc2 <nidhinc2@illinois.edu>
2023-09-09 18:07:59 -0500

    made some changes

--- app.py
+++ app.py
@@ -6,12 +6,9 @@ app = Flask(__name__)
 
 @app.route('/')
 #The first page that opens is the login page for the budget tracker. So an empty request renders index.html (which has the login stuff)
-def hello():
-    return render_template('index.html')
-
-@app.route('/login')
 def login():
-    return render_template('login.html')  
+    return render_template('login.html')
+
 
 @app.route('/about/')
 def about():
